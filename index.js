1c02e7c4e585c5414df4758f5c707c77ce17d20b
AckermanFernando <fernandoloiola03@gmail..com>
2022-08-25 20:11:55 -0300

    regex para pegar os links

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  return arrayResultados;
+}
+
 function tratarErro(erro) {
   throw new Error(chalk.red(erro));
 }
@@ -9,7 +19,7 @@ async function pegarArquivo(caminhoDoArquivo) {
   const encoding = "utf-8";
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(texto);
+    console.log(extraiLinks(texto));
   } catch (error) {
     tratarErro(error);
   } finally {
