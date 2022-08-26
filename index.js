b2eebd0fad490848c055a718370924d21a29dbc8
AckermanFernando <fernandoloiola03@gmail..com>
2022-08-26 13:35:31 -0300

    ler todos os arquivos de dentro de uma pasta

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import chalk from "chalk";
 import fs from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 function extraiLinks(texto) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
@@ -8,7 +15,9 @@ function extraiLinks(texto) {
   while ((temp = regex.exec(texto)) !== null) {
     arrayResultados.push({ [temp[1]]: temp[2] });
   }
-  return arrayResultados;
+  return arrayResultados.length === 0
+    ? chalk.red("Não há links")
+    : arrayResultados;
 }
 
 function tratarErro(erro) {
@@ -18,8 +27,11 @@ function tratarErro(erro) {
 async function pegarArquivo(caminhoDoArquivo) {
   const encoding = "utf-8";
   try {
-    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(extraiLinks(texto));
+    const caminhoAbsoluto = path.join(__dirname, caminhoDoArquivo);
+    const arquivos = await fs.promises.readdir(caminhoAbsoluto, { encoding });
+    const arrayLinks = await lerTextos(caminhoAbsoluto, arquivos, encoding);
+
+    return arrayLinks;
   } catch (error) {
     tratarErro(error);
   } finally {
@@ -27,7 +39,21 @@ async function pegarArquivo(caminhoDoArquivo) {
   }
 }
 
-pegarArquivo("./arquivos/texto1.md");
+async function lerTextos(caminhoDoArquivo, arquivosDeTexto, encoding) {
+  let listaDeLinks = [];
+  await Promise.all(
+    arquivosDeTexto.map(async (item) => {
+      let texto = await fs.promises.readFile(`${caminhoDoArquivo}${item}`, {
+        encoding,
+      });
+
+      listaDeLinks.push(extraiLinks(texto));
+    })
+  );
+  return listaDeLinks;
+}
+
+export default pegarArquivo;
 
 // Promise
 // return new Promise((resolve, reject) => {
