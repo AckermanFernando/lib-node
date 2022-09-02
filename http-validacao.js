2df14034024ec6613c2161a7d32794e9e670c59b
AckermanFernando <fernandoloiola03@gmail..com>
2022-09-02 18:33:45 -0300

    função para testar os links

--- http-validacao.js
+++ http-validacao.js
@@ -1,5 +1,29 @@
 import fetch from "node-fetch";
 
+function manejaErros(erro) {
+  throw new Error(errp.message);
+}
+
+async function checaStatus(arraysURLs) {
+  try {
+    const statusCodes = await Promise.all(
+      arraysURLs.map(async (arrayURL) => {
+        const arrayLinks = await Promise.all(
+          arrayURL.map(async (url) => {
+            const res = await fetch(url);
+            return `${res.status} - ${res.statusText}`;
+          })
+        );
+        return arrayLinks;
+      })
+    );
+
+    return statusCodes;
+  } catch (error) {
+    manejaErros(error);
+  }
+}
+
 function geraArrayDeURLs(arrayLinks) {
   return arrayLinks.map((arrayObjetos) =>
     arrayObjetos.map((arrayLink) => Object.values(arrayLink).join())
