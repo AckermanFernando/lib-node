f2d8710b1c5d8e23711deca53b6863ba7cc561dd
AckermanFernando <fernandoloiola03@gmail..com>
2022-09-02 18:33:15 -0300

    função para gerar um arraay com as urls

--- http-validacao.js
+++ http-validacao.js
@@ -1,1 +1,8 @@
 import fetch from "node-fetch";
+
+function geraArrayDeURLs(arrayLinks) {
+  return arrayLinks.map((arrayObjetos) =>
+    arrayObjetos.map((arrayLink) => Object.values(arrayLink).join())
+  );
+}
+
